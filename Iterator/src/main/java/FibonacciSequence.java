9c76d545b95724dc394128237773c3e59f3747ab
Teemu Erkkilä <teemu.erkkila@tuni.fi>
2025-10-02 14:59:06 +0300

    Add explaining comment to iterator

--- Iterator/src/main/java/FibonacciSequence.java
+++ Iterator/src/main/java/FibonacciSequence.java
@@ -1,7 +1,7 @@
 import java.util.Iterator;
 
 public class FibonacciSequence implements Sequence{
-
+    //Keep track of progress in iterator so separate iterators don't conflict with each other
     public Iterator<Integer> iterator(){
         return new FibonacciIterator(this);
     }
